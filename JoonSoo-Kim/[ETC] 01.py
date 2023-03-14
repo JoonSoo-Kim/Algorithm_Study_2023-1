9343e6f4879fa06ef5c6cf6bd60924eb812d6752
Joonsoo Kim <kjsoo1901@gmail.com>
2023-03-14 16:18:18 +0900

    [ETC] 유효한 팰린드롬 / -
    파이썬 알고리즘 인터뷰

--- JoonSoo-Kim/[ETC] 01.py
+++ JoonSoo-Kim/[ETC] 01.py
@@ -1,4 +1,5 @@
 import re
+import collections
 
 def isPalindrome(sentence):
     sentence = re.sub(r"[^a-zA-Z0-9]", "", sentence).lower()
@@ -7,6 +8,38 @@ def isPalindrome(sentence):
             print(sentence[i], sentence[len(sentence) - i - 1])
             return False
     return True
+
+# 리스트로 변환
+def isPalindromeExample01(self, sentence: str) -> bool:
+    processedSentence = []
+    for char in sentence:
+        if char.isalnum():
+            processedSentence.append(char.lower())
+
+    while len(processedSentence) > 1:
+        if processedSentence.pop(0) != processedSentence.pop():
+            return False
+    return True
+
+# 데크를 이용한 최적화
+def isPalindromeExample02(self, sentence: str) -> bool:
+    sentenceDeque = collections.deque()
+
+    for char in sentence:
+        if char.isalnum():
+            sentenceDeque.append(char.lower())
+
+    while len(sentenceDeque) > 1:
+        if sentenceDeque.popleft() != sentenceDeque.pop():
+            return False
+    return True
+
+# 슬라이싱 사용
+def isPalindromeExample03(self, sentence: str) -> bool:
+    sentence = sentence.lower()
+    sentence = re.sub("[^a-z0-9]", '', sentence)
+
+    return sentence == sentence[::-1]
 
 
 sentence = input()
