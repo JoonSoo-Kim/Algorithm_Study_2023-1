9126e7b9a15258e3ef3fec68024c663b01f2dcd5
Joonsoo Kim <kjsoo1901@gmail.com>
2023-03-14 16:34:45 +0900

    [ETC] 문자열 뒤집기 / -
    파이썬 알고리즘 인터뷰

--- JoonSoo-Kim/[ETC] 02.py
+++ JoonSoo-Kim/[ETC] 02.py
@@ -3,6 +3,19 @@ import re
 def reverseStringList(stringList: list) -> list:
     return stringList[::-1]
 
+# 투 포인터를 이용한 스왑
+def reverseStringListExample01(stringList: list) -> list:
+    left, right = 0, len(stringList) - 1;
+    while left < right:
+        stringList[left], stringList[right] = stringList[right], stringList[left]
+        left += 1
+        right -= 1
+    return stringList
+
+# 파이써닉 방식
+def reverseStringListExample02(stringList: list) -> list:
+    return stringList.reverse()
+
 
 strings = input()
 stringList = []
